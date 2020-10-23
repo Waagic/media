73c70dec0b23bd8768e77b8d1ebd4061f9c75eeb
Lucas Marguiron <lucas@mcube.fr>
2020-10-23 10:13:49 +0200

    :'(

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,5 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
+
+import './functions';
